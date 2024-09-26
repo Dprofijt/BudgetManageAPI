dc90757c16948a48b516967eaab61aae377e8e19
Dprofijt <44891295+Dprofijt@users.noreply.github.com>
2024-09-26 22:24:40 +0200

    just trying stuff not yet working

---
+++ BudgetManageAPI/Models/GenerateDtoFromModels.cs
@@ -0,0 +1,57 @@
+﻿ <!-- Reference the assembly directly -->
+
+Starting T4 template execution...
+Model namespace set to: BudgetManageAPI.Models
+DTO namespace set to: BudgetManageAPI.DTOs
+Listing all types in the BudgetManageAPI assembly:
+ReflectionTypeLoadException caught:
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'Microsoft.AspNetCore.Mvc.Core, Version=8.0.0.0, Culture=neutral, PublicKeyToken=adb9793829ddae60' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'Microsoft.AspNetCore.Mvc.Core, Version=8.0.0.0, Culture=neutral, PublicKeyToken=adb9793829ddae60' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'Microsoft.AspNetCore.Mvc.Core, Version=8.0.0.0, Culture=neutral, PublicKeyToken=adb9793829ddae60' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'Microsoft.AspNetCore.Mvc.Core, Version=8.0.0.0, Culture=neutral, PublicKeyToken=adb9793829ddae60' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'Microsoft.AspNetCore.Mvc.Core, Version=8.0.0.0, Culture=neutral, PublicKeyToken=adb9793829ddae60' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
+Loader Exception: Could not load file or assembly 'System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e' or one of its dependencies. Het systeem kan het opgegeven bestand niet vinden.
